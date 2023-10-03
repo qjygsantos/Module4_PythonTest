b92e0e8c250461ae6555e74438825ed0bd4f4083
qjygsantos <qjygsantos@tip.edu.ph>
2023-10-03 07:45:14 +0000

    programming act 4.1

---
+++ test.py
@@ -0,0 +1,23 @@
+import math
+
+def test_sqrt():
+    num = 25
+    assert math.sqrt(num) == 5
+    
+
+def test_square():
+    num = 7
+    assert num*num == 48
+    
+def test_check():
+    x = 1000
+    assert x >=100
+ 
+def test_fun():
+    x = 4
+    assert x+1 == 6
+    
+def test_multiply():
+    num1 = 4
+    num2 = 5
+    assert num1*num2 == 20
